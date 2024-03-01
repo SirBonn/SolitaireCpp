dd7eb1a05dce7a83db844058ee597d848dc3e029
SirBonn <byronvasquez21@gmail.com>
2024-03-01 02:32:57 -0600

    primer prueba funcionalidad

--- main.cpp
+++ main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
+#include "solitaire/Game.h"
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+
+    Game game;
+
+    std::cout << "filling" << std::endl;
+    game.fillCards();
+    std::cout << "Filled" << std::endl;
 
     return 0;
 }
